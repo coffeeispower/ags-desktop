075ebf88440e249db7e04b6a293f0d75dbba08fb
Tiago Dinis <tiagodinis33@proton.me>
2024-12-22 03:10:22 +0000

    chore: rename executable

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       default = ags.lib.bundle {
         inherit pkgs;
         src = ./.;
-        name = "bar";
+        name = "ags-desktop";
         entry = "app.ts";
 
         # additional libraries and executables to add to gjs' runtime
